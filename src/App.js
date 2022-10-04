3b7810591c6ffe27fee1df17b5c04590c61d99ec
nnmchau <19520023@gm.uit.edu.vn>
2022-10-04 09:21:22 +0700

    add

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function test(){
+    console.log('hahaa')
+  }
+  test();
+  }
   return (
     <div className="App">
       <header className="App-header">
