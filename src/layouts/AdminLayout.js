e7241a91aa8569e7f4ac1d8144603a7687ecdf42
DienChau <19521275@gm.uit.edu.vn>
2023-02-01 01:56:20 +0700

    responsive sidebar nav

--- src/layouts/AdminLayout.js
+++ src/layouts/AdminLayout.js
@@ -6,12 +6,20 @@ import ProtectedRoute from "../routes/PrivateRoute";
 //Đây là layout cho trang Quản lí
 export default function AdminLayout({ children }) {
   return (
-      <div style={{ display: "flex", width: "100%" }}>
-        <Sidebar />
-        <div style={{ flex: "4" }}>
-          <Navbar />
-          {children}
+    <div style={{ display: "flex", width: "100%" }}>
+      <div class="container-fluid">
+        <div class="row flex-nowrap">
+          <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0">
+            <Sidebar />
+          </div>
+          <div class="col py-3">
+            <div style={{ flex: "4" }}>
+              <Navbar />
+              {children}
+            </div>
+          </div>
         </div>
       </div>
+    </div>
   );
 }
