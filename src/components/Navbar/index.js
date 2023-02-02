6d5ad25915abf45efa82d5bfe715ac8c2a84389c
DienChau <19521275@gm.uit.edu.vn>
2023-02-02 13:50:18 +0700

    fixed ...

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,19 +1,18 @@
 import "./navbar.scss";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
-import { Button } from 'react-bootstrap';
-import SearchIcon from '@mui/icons-material/Search';
+import { Button } from "react-bootstrap";
+import SearchIcon from "@mui/icons-material/Search";
 import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
 import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
 import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
 import { useContext } from "react";
 
 const Navbar = () => {
-
-    return (
-        <div className="nav">
-            <div className="wrapper">
-                {/* <div className="col-xl-6 col-lg-5 col-md-6">
+  return (
+    <div className="nav">
+      <div className="wrapper">
+        {/* <div className="col-xl-6 col-lg-5 col-md-6">
                     <form action="#" className="search-header">
                         <div className="input-group w-100">
                             <input type="text" className="form-control" placeholder="Tìm kiếm" />
@@ -25,32 +24,43 @@ const Navbar = () => {
                         </div>
                     </form>
                 </div> */}
-                <marquee onMouseOver="this.stop()" onMouseOut="this.start()" scrollamount="10" className='ad-banner'>
-                    ❤️❤️❤️ <mark> Đây là dòng để cho bớt trống, xin đừng quá quan tâm về nó mà hãy quan tâm những thứ có ích bên dưới. Cám ơn!!! </mark> - <i> From toàn bộ thành viên nhóm 6 With Love </i>  ❤️❤️❤️
-                </marquee>
-                <div className="items">
-                    <div className="item">
-                        <NotificationsNoneOutlinedIcon className="icon" />
-                        <div className="counter">1</div>
-                    </div>
-                    <div className="item">
-                        <ChatBubbleOutlineOutlinedIcon className="icon" />
-                        <div className="counter">2</div>
-                    </div>
-                    <div className="item">
-                        <ListOutlinedIcon className="icon" />
-                    </div>
-                    <div className="item">
-                        <img
-                            src="https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-                            alt=""
-                            className="avatar"
-                        />
-                    </div>
-                </div>
-            </div>
+        <marquee
+          onMouseOver="this.stop()"
+          onMouseOut="this.start()"
+          scrollamount="10"
+          className="ad-banner"
+        >
+          {/* ❤️❤️❤️ <mark> Đây là dòng để cho bớt trống, xin đừng quá quan tâm về nó mà hãy quan tâm những thứ có ích bên dưới. Cám ơn!!! </mark> - <i> From toàn bộ thành viên nhóm 6 With Love </i>  ❤️❤️❤️ */}
+          <mark>
+            ❤️❤️❤️ This is a line to make it less blank, please don't be too
+            concerned about it but take care of the useful things below. Thank
+            you!!!{" "}
+          </mark>{" "}
+          - <i> From all members of group 4 With Love </i>
+        </marquee>
+        <div className="items">
+          <div className="item">
+            <NotificationsNoneOutlinedIcon className="icon" />
+            <div className="counter">1</div>
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineOutlinedIcon className="icon" />
+            <div className="counter">2</div>
+          </div>
+          <div className="item">
+            <ListOutlinedIcon className="icon" />
+          </div>
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              alt=""
+              className="avatar"
+            />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
